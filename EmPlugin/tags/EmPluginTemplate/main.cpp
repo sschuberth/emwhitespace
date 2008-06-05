0251bb94f5bbebfcd8ed929a90cf3050b83fd129
sschuberth <sschuberth@015cc09a-efeb-11dd-8c4e-4bbb75a408b7>
2008-06-05 22:29:05 +0000

    Create a template project for EmEditor plug-ins.

---
+++ EmPlugin/tags/EmPluginTemplate/main.cpp
@@ -0,0 +1,4 @@
+#include "main.h"
+
+// The following line is needed after CMyFrame definition.
+_ETL_IMPLEMENT
---
+++ EmPlugin/tags/EmPluginTemplate/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define IDS_MENU_TEXT                   101
+#define IDS_STATUS_MESSAGE              102
+#define IDS_VERSION                     103
+#define IDS_SURE_TO_UNINSTALL           104
+#define IDB_24_24_24                    201
+#define IDB_24_24_24_BW                 202
+#define IDB_24_24_24_HOT                203
+#define IDB_24_24_8                     204
+#define IDB_24_24_8_BW                  205
+#define IDB_24_24_8_HOT                 206
+#define IDB_24_24_4                     207
+#define IDB_16_16_24                    208
+#define IDB_16_16_24_BW                 209
+#define IDB_16_16_24_HOT                210
+#define IDB_16_16_8                     211
+#define IDB_16_16_8_BW                  212
+#define IDB_16_16_8_HOT                 213
+#define IDB_16_16_4                     214
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
